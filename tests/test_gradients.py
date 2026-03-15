6979097a75d081a6052775ef1db13987de1e4d32
Gunnar Voet <gvoet@ucsd.edu>
2026-03-15 16:44:53 -0700

    feat: add horizontal_gradient (frozen-field dT/dx)

--- tests/test_gradients.py
+++ tests/test_gradients.py
@@ -43,3 +43,23 @@ def test_vertical_gradient_bottom_sensor():
 
     dtdz_mean, dtdz_ts = vertical_gradient(temp_cal, depths, sensor_index=2)
     assert dtdz_mean == pytest.approx(-0.1)
+
+
+from pychi.gradients import horizontal_gradient
+
+
+def test_horizontal_gradient_known_values():
+    """Frozen-field dT/dx: (T_end - T_start) / (dt * U)."""
+    temp_chunk = np.array([10.0, 10.5, 11.0, 11.5, 12.0])
+    chunk_duration_s = 600.0
+    U = 0.5
+
+    dtdx = horizontal_gradient(temp_chunk, chunk_duration_s, U)
+    assert dtdx == pytest.approx(2.0 / 300.0)
+
+
+def test_horizontal_gradient_negative():
+    """Cooling trend produces negative dT/dx."""
+    temp_chunk = np.array([15.0, 14.0, 13.0])
+    dtdx = horizontal_gradient(temp_chunk, 600.0, 1.0)
+    assert dtdx == pytest.approx(-2.0 / 600.0)
